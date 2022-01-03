fe0216ca19223274b8ea6742decdb397e7bd7d80
QuantZero <yigitkasapoglu95@gmail.com>
2022-01-03 19:52:22 +0300

    Dog Age Calculator Added

---
+++ JS-Exercise/dogAgeCalculator.js
@@ -0,0 +1,7 @@
+//Copy and paste this code below to your console and find out your dog's age with just one click!
+
+var dogAge = prompt("Enter your dog's age")
+
+var humanAge=(dogAge-2)*4+21
+
+alert("Your dog is " + humanAge + " years old in human years.");
