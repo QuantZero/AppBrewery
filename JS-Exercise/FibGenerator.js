6d592a02a0c18b3b22969a7ab26f304b23b1da2a
QuantZero <yigitkasapoglu95@gmail.com>
2022-01-08 23:10:17 +0300

    Fibonacci Sequence Generator added.

---
+++ JS-Exercise/FibGenerator.js
@@ -0,0 +1,29 @@
+//This function will generate a fibonacci sequance count based on the number you enter inside function. Try fibonacciGenerator(555) as an example.
+
+function fibonacciGenerator(n) {
+
+    var output = [];
+    
+    if(n===1) {
+      output = [0];
+    }
+    else if (n===2) {
+      output = [0,1];
+    }
+    else {
+      output=[0,1];
+    
+      for (var i=2; i<n; i++) {
+        output.push(output[output.length-2]+output[output.length-1]);
+      }
+      
+    }
+    
+    return output;
+    
+    
+    }
+    
+    output=fibonacciGenerator(555);
+    console.log(output);
+    
