b3b47f074f6792844383661adbc9bcc15b3c24b9
QuantZero <yigitkasapoglu95@gmail.com>
2022-01-22 07:36:30 +0300

    Node.js Module Test

---
+++ IntroToNode/index.js
@@ -0,0 +1,11 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var badName = supervillains.random();
+
+var sprName = superheroes.random();
+
+
+
+console.log(sprName);
+console.log(badName);
