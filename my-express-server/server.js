960e73e0701a0e08294d1d7850bf8492a02499e8
QuantZero <yigitkasapoglu95@gmail.com>
2022-01-23 19:29:02 +0300

    Added express server test abd Calculator Project v1.0.0

---
+++ my-express-server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(request, response) {
+    console.log(request);
+    response.send("Hello Wurld");
+});
+
+app.get("/about", function(req, res){
+    res.send("God-like Pickles Street 32/1")
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000.")
+});
