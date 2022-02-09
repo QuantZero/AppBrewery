6de8a6acc90dcb38a5cf5d1004a356fa3d01f639
QuantZero <yigitkasapoglu95@gmail.com>
2022-02-09 19:00:38 +0300

    Restructured files.

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,25 +3,53 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js")
+const mongoose = require("mongoose");
 
 const app = express();
 
-let items = ["Buy Food", "Get Beer", "Hit the Gym"];
-let workItems = [];
-
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-    extended: true
-})); //added bodyParser
+app.use(bodyParser.urlencoded({extended: true})); //added bodyParser
 app.use(express.static("public"));
+
+mongoose.connect("mongoose://localhost:27017/todolistDB")
+
+const itemsSchema = {
+    name: String
+
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item ({
+    name: "Welcome to your todolist!"
+});
+
+const item2 = new Item ({
+    name: "Hit the plus button to add a new item."
+});
+
+const item3 = new Item ({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(err){
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("Successfully saved default items");
+    }
+});
 
 app.get("/", function (req, res) {
 
 let day = date();
 
     res.render("list", {
-        listTitle: day,
+        listTitle: "Today",
         newListItems: items
     }); //respond by rendering  list esj
 
